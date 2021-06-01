9d68cddf6b2253086b2f4e197ba5272054c76879
FuckAndPaizao <82721116+FuckAndPaizao@users.noreply.github.com>
2021-06-01 13:35:52 -0300

    Add files via upload

---
+++ src/aluguelbot.js
@@ -0,0 +1,20 @@
+const aluguelbot = (prefix) => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *http://wa.me/+351926456359*
+║
+╚═〘  CAUSS  〙
+`
+}
+exports.iklan = aluguelbot
+
+
