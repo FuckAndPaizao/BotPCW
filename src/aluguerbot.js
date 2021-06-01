279a2c3e894b5b8dcbcee1f593a8113ccbd3fe95
FuckAndPaizao <82721116+FuckAndPaizao@users.noreply.github.com>
2021-06-01 14:13:03 -0300

    Add files via upload

---
+++ src/aluguerbot.js
@@ -0,0 +1,21 @@
+const aluguelbot = (prefix) => { 
+	return `           
+╔══✪〘 ⏤͟͟͞͞𝑭𝑼𝑪𝑲⛧𝑻𝑹𝑬𝑴✘ 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *http://wa.me/+351926456359*
+║
+╚═〘  ⏤͟͟͞͞𝑭𝑼𝑪𝑲⛧𝑻𝑹𝑬𝑴✘  〙
+`
+}
+exports.aluguelbot = aluguelbot
+
+
+
