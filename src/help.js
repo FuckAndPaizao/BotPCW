fda5c4ab41617203711f027bf6d5a4aba396d1f1
FuckAndPaizao <82721116+FuckAndPaizao@users.noreply.github.com>
2021-06-01 12:55:08 -0300

    Add files via upload

--- src/help.js
+++ src/help.js
@@ -1,85 +1,87 @@
 const help = (prefix) => {
 	return `
-⛥ ═════════════════════ ⛥
-          ⏤͟͞𝐁𝐎𝐓⛧𝐏𝐂𝐖✘
-⛥ ═════════════════════ ⛥ 
+⛥ ═══════════════════ ⛥
+                 ⏤͟͞𝐁𝐎𝐓⛧𝐏𝐂𝐖✘
+⛥ ═══════════════════ ⛥ 
      
-──▄────▄▄▄▄▄▄▄────▄───
-─▀▀▄─▄█████████▄─▄▀▀──
-─────██─▀███▀─██──────
-───▄─▀████▀████▀─▄────
-─▀█────██▀█▀██────█▀──
+ ──▄────▄▄▄▄▄▄▄────▄───
+ ─▀▀▄─▄█████████▄─▄▀▀──
+ ─────██─▀███▀─██──────
+ ───▄─▀████▀████▀─▄────
+ ─▀█────██▀█▀██────█▀──
 
-*DONO: ⏤͟͟͞͞𝑭𝑼𝑪𝑲⛧𝑻𝑹𝑬𝑴✘*
-*BOT: 𝑷𝑪𝑾¹⁵⁷ V1*
-┌────────1─────────
-║      〘 *INFORMAÇÕES* 〙
+
+━━━━━━━✦✗✦━━━━━━━━
+DONO: ⏤͟͟͞͞𝑭𝑼𝑪𝑲⛧𝑻𝑹𝑬𝑴✘
+BOT: 𝑷𝑪𝑾¹⁵⁷ V1
+━━━━━━━✦✗✦━━━━━━━━
+
+┌────────❶─────────
+║      〘 INFORMAÇÕES 〙
 ╠🛠 ⏤͟͞𝐁𝐎𝐓⛧𝐏𝐂𝐖✘
 ╠🛠 𝐃𝐎𝐍𝐎:⏤͟͟͞͞𝑭𝑼𝑪𝑲⛧𝑻𝑹𝑬𝑴✘
 ╠🛠 http://wa.me/351926456359
-╠🛠 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+╠🛠 𝐒𝐓𝐀𝐓𝐔𝐒: _ON_
 ║
 ╠═══⚔️ 〘 Tag/Recado 〙⚔️════
-╠➥ *tag*
-╠➥ *recado*
+╠➥ *tag
+╠➥ *recado
 ║
 ╠═══⚔️ 〘 MENU 〙⚔️════
-╠➥ *${prefix}sticker*
-╠➥ *${prefix}converter* ( converter fig em imagem )
-╠➥ *${prefix}txtfig*
+╠➥ *sticker
+╠➥ *converter ( converter fig em imagem )
+╠➥ *txtfig
 ║
 ╠═══⚔️〘 GRUPO 〙⚔️════
-╠➥ *${prefix}antlink [1/0] [somente adm]*
-╠➥ *${prefix}antracismo [on/off] [somente adm]*
-╠➥ *${prefix}admins* 
-╠➥ *${prefix}base [somente adm]*
-╠➥ *${prefix}banir [@] [somente adm]*
-╠➥ *${prefix}bemvindo [1/0] [somente adm]*
-╠➥ *${prefix}linkgp [somente adm]* 
-╠➥ *${prefix}pall [somente adm]*
-╠➥ *${prefix}pall2 [somente adm]*
-╠➥ *${prefix}pall3 [somente adm]*
-╠➥ *${prefix}promover [@] [somente adm]*
-╠➥ *${prefix}rebaixar [@] [somente adm]*
-╠➥ *${prefix}setnome [somente adm]*
-╠➥ *${prefix}setfoto [na legenda] - [somente adm]*
+╠➥ *antlink [1/0] [somente adm]
+╠➥ *antracismo [on/off] [somente adm]
+╠➥ *admins 
+╠➥ *base [somente adm]
+╠➥ *banir [@] [somente adm]
+╠➥ *bemvindo [1/0] [somente adm]
+╠➥ *linkgp [somente adm] 
+╠➥ *pall [somente adm]
+╠➥ *pall2 [somente adm]
+╠➥ *pall3 [somente adm]
+╠➥ *promover [@] [somente adm]
+╠➥ *rebaixar [@] [somente adm]
+╠➥ *setnome [somente adm]
+╠➥ *setfoto [na legenda] - [somente adm]
 ║
 ╠═══⚔️〘 OUTROS 〙⚔️════
-╠➥ *${prefix}bot*
-╠➥ *${prefix}canal*
-╠➥ *${prefix}lofi*
-╠➥ *${prefix}pcwinsta*
-╠➥ *${prefix}termux*
-╠➥ *${prefix}wa.me*
+╠➥ *bot
+╠➥ *canal
+╠➥ *lofi
+╠➥ *pcwinsta
+╠➥ *termux
+╠➥ *wa.me
 ║
 ╠═══⚔️〘 BOT INFOS〙⚔️════
-╠➥ *${prefix}ping [ver velocidade do bot]*
-╠➥ *${prefix}dono*
+╠➥ *ping [ver velocidade do bot]
+╠➥ *dono
 ║
 ╠═══⚔️〘 INTERATIVOS 〙⚔️════
-╠➥ *bot*
-╠➥ *canal*
-╠➥ *eae*
-╠➥ *fdp*
-╠➥ *kdobot*
-╠➥ *obgd*
-╠➥ *pcw*
-╠➥ *vtmnc*
-╠➥ *vsfd*
+╠➥ bot
+╠➥ canal
+╠➥ eae
+╠➥ fdp
+╠➥ kdobot
+╠➥ obgd
+╠➥ pcw
+╠➥ vtmnc
+╠➥ vsfd
 ║
 ╠═══⚔️ 〘 DONO 〙⚔️════
-╠➥ *${prefix}ptm* (tm de avisa)
-╠➥ *${prefix}pt2* (tm de divulgação)
-╠➥ *${prefix}pt3* (tm admin)
+╠➥ *ptm (tm de avisa)
+╠➥ *pt2 (tm de divulgação)
+╠➥ *pt3 (tm admin)
 ║
-╠═══ ⚔️ 〘 PCW NO CONTROLE 〙⚔️════
+╠═══ ⚔️ 〘 INFOS PCW 〙⚔️════
 ║
 ║    𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
-║               ⏤͟͞𝐁𝐎𝐓⛧𝐏𝐂𝐖✘                  
+║                       ⏤͟͞𝐁𝐎𝐓⛧𝐏𝐂𝐖✘                  
 ║    𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
 ║
-╠➥ Canal de Edit: 
-║https://youtube.com/channel/UCnzvrzBTDzzycCU0JxWah_g 
 ║
 ╠➥ Canal da MaryHarphya:
 ║https://youtube.com/channel/UCeYswOOlot2mKghfcmj2VkQ 
@@ -88,10 +90,11 @@ const help = (prefix) => {
 ║https://youtube.com/channel/UCCXSfIsQVm1w1udGNW5hX5A
 ║
 ║
-╚═〘 ⏤͟͞𝐁𝐎𝐓⛧𝐏𝐂𝐖✘ 〙`
+╚══════〘 ⏤͟͞𝐁𝐎𝐓⛧𝐏𝐂𝐖✘ 〙`
 }
 
 exports.help = help
+
 
 
 
