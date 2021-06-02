8c6c4d8061309a8636b2ab06bf06d2fa70d7f9b7
FuckAndPaizao <82721116+FuckAndPaizao@users.noreply.github.com>
2021-06-02 16:28:50 -0300

    Add files via upload

---
+++ src/daftatvip.js
@@ -0,0 +1,20 @@
+const daftatvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 13 > Recursos VIP + Insira o bot no seu grupo!
+-Rp. 3 > Adicionar bot em grupo
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+wa.me/351926456359
+
+*NOTA*
+
+*GRUPO DO ⏤͟͞𝐁𝐎𝐓⛧𝐏𝐂𝐖✘:* `
+}
+exports.daftatvip = daftatvip
